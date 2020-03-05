6df5be1f3e67da86982ebf60a249da5eee9dda0b
Bunsarak PEN <penbunsarak@gmail.com>
2020-03-05 14:44:01 +0100

    tab4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  }, {
+    path: '**',
+    redirectTo: '/tabs/tab3',
   }
+
 ];
 @NgModule({
   imports: [
